f6fca5f5e7b19b0ebda54f7ffc12187ba8955385
Caroline Seip <seip@unbc.ca>
2018-11-26 23:30:13 -0800

    update tsv

--- firstletter.R
+++ firstletter.R
@@ -3,4 +3,6 @@ library(tibble)
 words <- readLines("words.txt") #read in .txt file
 first <- substr(words, 1, 1) #extract first letter of each word
 lowfirst <- tolower(first) #convert letters to lower case
-write.table(lowfirst, "firstletter.tsv")
+firstlettertable <- table(lowfirst)
+firstlettertable
+write.table(firstlettertable, "firstletter.tsv")
