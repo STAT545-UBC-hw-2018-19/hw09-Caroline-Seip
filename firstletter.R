4a211f73769caf3bb2517be5a27440dfca1e9eda
Caroline Seip <seip@unbc.ca>
2018-11-27 00:16:23 -0800

    barplot

--- firstletter.R
+++ firstletter.R
@@ -1,5 +1,12 @@
+library(ggplot2)
+library(tidyverse)
 words <- readLines("words.txt") #read in .txt file
 first <- substr(words, 1, 1) #extract first letter of each word
 lowfirst <- tolower(first) #convert letters to lower case
 firstlettertable <- table(lowfirst) #create frequency table of first letters
 write.table(firstlettertable, "firstletter.tsv") #output table to .tsv
+png(filename = "barplot.png", width = 500, height = 500)
+barplot(firstlettertable, main = "Number of words that start with each letter",
+				xlab= "Letter")
+
+				
