7e84008cb405ce67bff5142981b8028df95f57c7
Caroline Seip <seip@unbc.ca>
2018-11-26 23:32:34 -0800

    Update script

--- firstletter.R
+++ firstletter.R
@@ -1,8 +1,5 @@
-library(tidyverse)
-library(tibble)
 words <- readLines("words.txt") #read in .txt file
 first <- substr(words, 1, 1) #extract first letter of each word
 lowfirst <- tolower(first) #convert letters to lower case
-firstlettertable <- table(lowfirst)
-firstlettertable
-write.table(firstlettertable, "firstletter.tsv")
+firstlettertable <- table(lowfirst) #create frequency table of first letters
+write.table(firstlettertable, "firstletter.tsv") #output table to .tsv
